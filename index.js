5d198371b5afc2e67a770474fd303e6c8b47f8bc
bengroves <bengroves15@gmail.com>
2021-01-27 14:49:34 -0500

    commit tree 3806379d92ac150fc12e992576f837b974547482

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    changeMessage()
+  }); 
+
+  function changeMessage () {
+    let newMessage = document.getElementById('text');
+    newMessage.innerHTML = "This is really cool!";
+    }
